10fdb886b2b44c8e27ad9d1a1aa2e81405b886dc
Sergey Petrochenko <proffsneip@gmail.com>
2018-10-30 21:23:18 +0300

    BT-2: added generic repository.

---
+++ BestTours/BT.DataAccess/Repositories/Implementation/UserRepository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BT.DataAccess.Repositories.Implementation
+{
+    public class UserRepository<T> : IRepository<T> where T: class 
+    {
+
+
+        public UserRepository()
+        {
+            
+        }
+
+        public T GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IReadOnlyCollection<T> FindBy(Func<T, bool> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Create(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
