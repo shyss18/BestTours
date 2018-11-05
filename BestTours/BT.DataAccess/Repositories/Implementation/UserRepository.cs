b1ef3cf9cbb9f500c3b1f643b419659c00196fdb
Sergey Petrochenko <proffsneip@gmail.com>
2018-11-05 14:46:37 +0300

    BT-2: created implementation UserRepository.

--- BestTours/BT.DataAccess/Repositories/Implementation/UserRepository.cs
+++ BestTours/BT.DataAccess/Repositories/Implementation/UserRepository.cs
@@ -1,43 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using BT.DataAccess.Context;
+using BT.Dom.Entities;
 
 namespace BT.DataAccess.Repositories.Implementation
 {
-    public class UserRepository<T> : IRepository<T> where T: class 
+    public class UserRepository : IUserRepository
     {
-
+        private ApplicationContext _db;
 
         public UserRepository()
         {
-            
+            _db = new ApplicationContext();
         }
 
-        public T GetById(int id)
+        public User GetById(int? id)
+        {
+            var user = _db.Users.Where(model => model.Id == id).Select(m => m).FirstOrDefault();
+
+            return user;
+        }
+
+        public ICollection<User> FindBy(Func<User, bool> predicate)
+        {
+            var users = _db.Users.Where(predicate).Select(m => m).ToList();
+
+            return users;
+        }
+
+        public void CreateUser(User item)
+        {
+            _db.Users.Add(item);
+        }
+
+        public void UpdateUser(User item)
         {
             throw new NotImplementedException();
         }
 
-        public IReadOnlyCollection<T> FindBy(Func<T, bool> predicate)
+        public void DeleteUser(int id)
         {
-            throw new NotImplementedException();
-        }
+            var user = _db.Users.Where(m => m.Id == id).Select(m => m).FirstOrDefault();
 
-        public void Create(T item)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Update(T item)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Delete(int id)
-        {
-            throw new NotImplementedException();
+            if (user != null)
+            {
+                _db.Users.Remove(user);
+                _db.SaveChanges();
+            }
         }
     }
 }
