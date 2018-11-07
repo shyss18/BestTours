4f4c8513013502ac95dc4e94f5f43c5d029f847c
Sergey Petrochenko <proffsneip@gmail.com>
2018-11-07 21:06:57 +0300

    BT-4: added config files.

--- BestTours/BT.Web/App_Start/RouteConfig.cs
+++ BestTours/BT.Web/App_Start/RouteConfig.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace BT.Web
@@ -17,6 +13,26 @@ namespace BT.Web
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "Login",
+                url: "Account/Login"
+            );
+
+            routes.MapRoute(
+                name: "MainPage",
+                url: "Home/Index"
+            );
+
+            routes.MapRoute(
+                name: "LogOut",
+                url: "Account/LogOut"
+            );
+
+            routes.MapRoute(
+                name: "Register",
+                url: "Account/Register"
             );
         }
     }
--- BestTours/BT.Web/Global.asax.cs
+++ BestTours/BT.Web/Global.asax.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
+using System.Web.Optimization;
+using BT.Web.App_Start;
 
 namespace BT.Web
 {
@@ -13,6 +11,7 @@ namespace BT.Web
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
