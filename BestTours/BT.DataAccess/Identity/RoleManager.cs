5fca6a86e67366839e13ef1593bc3a5fd5b70a62
Sergey Petrochenko <proffsneip@gmail.com>
2018-11-07 14:06:35 +0300

    BT-2: created entities manager.

---
+++ BestTours/BT.DataAccess/Identity/RoleManager.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNet.Identity;
+using BT.Dom.Entities;
+
+namespace BT.DataAccess.Identity
+{
+    public class RoleManager : RoleManager<Role>
+    {
+        public RoleManager(IRoleStore<Role, string> store) : base(store)
+        { }
+    }
+}
