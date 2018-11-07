48b817c8603a53052039c6da81164fa387d6602a
Sergey Petrochenko <proffsneip@gmail.com>
2018-11-07 14:26:11 +0300

    BT-2: added dll.

---
+++ BestTours/BT.Web/App_Start/Startup.cs
@@ -0,0 +1,25 @@
+﻿using BT.Dom.Context;
+using BT.Dom.Manager;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+
+[assembly: OwinStartup(typeof(BT.Web.App_Start.Startup))]
+
+namespace BT.Web.App_Start
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.CreatePerOwinContext<ApplicationContext>(ApplicationContext.Create);
+            app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Account/Login"),
+            });
+        }
+    }
+}
