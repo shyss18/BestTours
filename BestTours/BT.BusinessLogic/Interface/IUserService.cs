9ca4448779510ec165dc5511838121e04a1713ca
Sergey Petrochenko <proffsneip@gmail.com>
2018-11-07 15:31:00 +0300

    BT-3: added user service.

---
+++ BestTours/BT.BusinessLogic/Interface/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using BT.BusinessLogic.DTO;
+using BT.BusinessLogic.Infrastructure;
+
+namespace BT.BusinessLogic.Interface
+{
+    public interface IUserService : IDisposable
+    {
+        Task<OperationDetails> Create(UserDTO userDto);
+        Task<ClaimsIdentity> Authenticate(UserDTO userDto);
+        Task SetInitialData(UserDTO adminDto, List<string> roles);
+    }
+}
