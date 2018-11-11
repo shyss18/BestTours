4d3a65abaa4e67740b7e697a3b10750230ffb49e
Sergey Petrochenko <proffsneip@gmail.com>
2018-11-11 16:11:06 +0300

    BT-5: updated user context.

---
+++ BestTours/BT.DataAccess/Context/UserContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+using BT.Dom.Entities;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace BT.DataAccess.Context
+{
+    public class UserContext : IdentityDbContext<User>
+    {
+        public UserContext(string connectionString) : base(connectionString)
+        { }
+
+        public DbSet<ClientProfile> ClientProfiles { get; set; }
+    }
+}
