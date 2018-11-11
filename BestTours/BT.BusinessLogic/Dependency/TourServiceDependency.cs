50891add12fcab71bdca4bdd773835f38ccbdf92
Sergey Petrochenko <proffsneip@gmail.com>
2018-11-11 16:12:21 +0300

    BT-5: created dependecy.

---
+++ BestTours/BT.BusinessLogic/Dependency/TourServiceDependency.cs
@@ -0,0 +1,15 @@
+﻿using BT.BusinessLogic.Interface;
+using BT.BusinessLogic.Services;
+using Ninject.Modules;
+
+namespace BT.BusinessLogic.Dependency
+{
+    public class TourServiceDependency : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<ITourService>().To<TourService>();
+
+        }
+    }
+}
