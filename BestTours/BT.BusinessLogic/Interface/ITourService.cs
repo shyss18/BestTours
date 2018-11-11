9b42d26bf754d77acc7786787de1fb15ed786017
Sergey Petrochenko <proffsneip@gmail.com>
2018-11-11 16:11:48 +0300

    BT-5: created service for tour.

---
+++ BestTours/BT.BusinessLogic/Interface/ITourService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using BT.Dom.Entities;
+
+namespace BT.BusinessLogic.Interface
+{
+    public interface ITourService
+    {
+        void CreateTour(Tour tour);
+        void DeleteTour(int? id);
+        void EditTour(Tour tour);
+        Tour GetById(int? id);
+        IEnumerable<Tour> GetAll();
+    }
+}
---
+++ BestTours/BT.BusinessLogic/Services/TourService.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using BT.BusinessLogic.Interface;
+using BT.DataAccess.Dependency;
+using BT.DataAccess.Interfaces;
+using BT.DataAccess.Repositories;
+using BT.Dom.Entities;
+using Ninject;
+
+namespace BT.BusinessLogic.Services
+{
+    public class TourService: ITourService
+    {
+        private readonly ITourRepository _tourRepository;
+
+        public TourService()
+        {
+            IKernel kernel = new StandardKernel(new TourDependency());
+            _tourRepository = kernel.Get<TourRepository>();
+        }
+
+        public void CreateTour(Tour tour)
+        {
+            _tourRepository.CreateTour(tour);
+        }
+
+        public void DeleteTour(int? id)
+        {
+            _tourRepository.DeleteTour(id);
+        }
+
+        public void EditTour(Tour tour)
+        {
+            _tourRepository.EditTour(tour);
+        }
+
+        public Tour GetById(int? id)
+        {
+            return _tourRepository.GetById(id);
+        }
+
+        public IEnumerable<Tour> GetAll()
+        {
+            return _tourRepository.GetAll();
+        }
+    }
+}
