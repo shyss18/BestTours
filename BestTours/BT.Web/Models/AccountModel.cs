54ba398bf2b2f67126333addb9864b0b432a6225
Sergey Petrochenko <proffsneip@gmail.com>
2018-11-13 18:17:31 +0300

    BT-5: created account model.

---
+++ BestTours/BT.Web/Models/AccountModel.cs
@@ -0,0 +1,15 @@
+﻿namespace BT.Web.Models
+{
+    public class AccountModel
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string NickName { get; set; }
+
+        public decimal Amount { get; set; }
+
+        public string Email { get; set; }
+    }
+}
