a69d6085e6d3dc3f6c5be6a728457cf100b04348
Sergey Petrochenko <proffsneip@gmail.com>
2018-11-19 21:50:18 +0300

    BT-6: added dependencies register.

---
+++ BestTours/BT.BusinessLogic/Dependency/TourServiceModule.cs
@@ -0,0 +1,14 @@
+﻿using BT.BusinessLogic.Interface;
+using BT.BusinessLogic.Services;
+using Ninject.Modules;
+
+namespace BT.BusinessLogic.Dependency
+{
+    public class TourServiceModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<ITourService>().To<TourService>();
+        }
+    }
+}
---
+++ BestTours/BT.BusinessLogic/Dependency/UserServiceModule.cs
@@ -0,0 +1,14 @@
+﻿using BT.BusinessLogic.Interface;
+using BT.BusinessLogic.Services;
+using Ninject.Modules;
+
+namespace BT.BusinessLogic.Dependency
+{
+    public class UserServiceModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IUserService>().To<UserService>();
+        }
+    }
+}
