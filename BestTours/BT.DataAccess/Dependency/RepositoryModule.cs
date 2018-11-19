d7646c4ae6a64fd5a80b952eac9adff124802c86
Sergey Petrochenko <proffsneip@gmail.com>
2018-11-19 21:51:03 +0300

    BT-6: added dependecies.

---
+++ BestTours/BT.DataAccess/Dependency/RepositoryModule.cs
@@ -0,0 +1,14 @@
+﻿using BT.DataAccess.Interfaces;
+using BT.DataAccess.Repositories;
+using Ninject.Modules;
+
+namespace BT.DataAccess.Dependency
+{
+    public class RepositoryModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<ITourRepository>().To<TourRepository>();
+        }
+    }
+}
---
+++ BestTours/BT.DataAccess/Dependency/UnitOfWorkModule.cs
@@ -0,0 +1,14 @@
+﻿using BT.DataAccess.Interfaces;
+using BT.DataAccess.Repositories;
+using Ninject.Modules;
+
+namespace BT.DataAccess.Dependency
+{
+    public class UnitOfWorkModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IUnitOfWork>().To<UnitOfWork>().WithConstructorArgument("connectionString", "IdentityDb");
+        }
+    }
+}
