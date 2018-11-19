3fe98ddc25da491e2089da5e84d6f37849d947de
Sergey Petrochenko <proffsneip@gmail.com>
2018-11-19 21:44:16 +0300

    BT-6: deleted service creator.

--- BestTours/BT.Web/App_Start/Startup.cs
+++ BestTours/BT.Web/App_Start/Startup.cs
@@ -11,20 +11,13 @@ namespace BT.Web.App_Start
 {
     public class Startup
     {
-        IServiceCreator serviceCreator = new ServiceCreator();
         public void Configuration(IAppBuilder app)
         {
-            app.CreatePerOwinContext<IUserService>(CreateUserService);
             app.UseCookieAuthentication(new CookieAuthenticationOptions
             {
                 AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
                 LoginPath = new PathString("/Account/Login"),
             });
-        }
-
-        private IUserService CreateUserService()
-        {
-            return serviceCreator.CreateUserService("IdentityDb");
         }
     }
 }
