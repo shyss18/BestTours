9089b455ed0263365bba272ca3e7e81235546a25
Sergey Petrochenko <proffsneip@gmail.com>
2018-11-21 18:45:00 +0300

    BT-6: added register validation.

--- BestTours/BT.Web/Models/RegisterModel.cs
+++ BestTours/BT.Web/Models/RegisterModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
 
 namespace BT.Web.Models
 {
@@ -7,6 +8,7 @@ namespace BT.Web.Models
         [Required(ErrorMessage = "Поле не заполнено!")]
         [DataType(DataType.EmailAddress)]
         [Display(Name = "Email")]
+        [Remote("CheckUserEmail", "Account")]
         public string Email { get; set; }
 
         [Required(ErrorMessage = "Поле не заполнено!")]
@@ -20,19 +22,20 @@ namespace BT.Web.Models
         [Required(ErrorMessage = "Поле не заполнено!")]
         [DataType(DataType.Password)]
         [Display(Name = "Пароль")]
-        [RegularExpression("^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\\s).*$", ErrorMessage = "Пароль должен содержать символы верхнего и нижнего регистров и не менее одной цифры")]
+        [RegularExpression("^(?=.*\\d)(?=.*[a-z])(?!.*\\s).*$", ErrorMessage = "Пароль должен содержать символы верхнего и нижнего регистров и не менее одной цифры")]
         [StringLength(20, MinimumLength = 6, ErrorMessage = "Пароль должен содержать от 6 до 20 символов")]
         public string Password { get; set; }
 
         [Required(ErrorMessage = "Поле не заполнено!")]
         [DataType(DataType.Password)]
-        [Compare("Password")]
+        [System.ComponentModel.DataAnnotations.Compare("Password")]
         [Display(Name = "Подтверждение пароля")]
         public string ConfirmPassword { get; set; }
 
         [Required(ErrorMessage = "Поле не заполнено!")]
         [Display(Name = "Никнейм")]
         [RegularExpression("^[A-Za-z0-9_]{1,15}$", ErrorMessage = "Неверное имя пользователя")]
+        [Remote("CheckUserName", "Account")]
         public string Name { get; set; }
     }
 }
