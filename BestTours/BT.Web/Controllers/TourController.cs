910a6caba8351b3b21a3dea117c0d8a3f58cf145
Sergey Petrochenko <proffsneip@gmail.com>
2018-11-22 00:21:39 +0300

    BT-6: created validation tour.

--- BestTours/BT.Web/Controllers/TourController.cs
+++ BestTours/BT.Web/Controllers/TourController.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Web.Mvc;
 using BT.BusinessLogic.Interface;
 using BT.Dom.Entities;
@@ -32,9 +33,15 @@ namespace BT.Web.Controllers
 
         [Authorize(Roles = "admin")]
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult AddTour(Tour model)
         {
-            _tourService.CreateTour(model);
+            if (ModelState.IsValid)
+            {
+                _tourService.CreateTour(model);
+
+                return RedirectToAction("Index", "Tour");
+            }       
 
             return View(model);
         }
@@ -54,33 +61,26 @@ namespace BT.Web.Controllers
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult EditTour(Tour model)
         {
-            _tourService.EditTour(model);
+            if (ModelState.IsValid)
+            {
+                _tourService.EditTour(model);
 
-            return RedirectToAction("Index");
+                return RedirectToAction("Index");
+            }
+
+            return View(model);
         }
 
         [Authorize(Roles = "admin")]
-        [HttpGet]
+        [HttpPost, ActionName("DeleteTour")]
         public ActionResult DeleteTour(int? id)
         {
             if (id == null)
             {
-                return HttpNotFound();
-            }
-
-            var tour = _tourService.GetById(id);
-
-            return View(tour);
-        }
-
-        [HttpPost, ActionName("DeleteTour")]
-        public ActionResult DeleteTourConfirmed(int? id)
-        {
-            if (id == null)
-            {
-                return HttpNotFound();
+                return View("Error");
             }
 
             _tourService.DeleteTour(id);
@@ -126,12 +126,45 @@ namespace BT.Web.Controllers
         {
             if (id == null)
             {
-                return HttpNotFound("404");
+                return View("Error");
             }
 
             var user = _userService.GetById(id);
             
             return View(user);
+        }
+
+        [HttpGet]
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return View("Error");
+            }
+
+            var tour = _tourService.GetById(id);
+
+            return View(tour);
+        }
+
+        public JsonResult ValidateDate(string Date)
+        {
+            DateTime parsedDate;
+
+            if (!DateTime.TryParse(Date, out parsedDate))
+            {
+                return Json("Пожалуйста, введите дату в формате (мм.дд.гггг)",
+                    JsonRequestBehavior.AllowGet);
+            }
+            else if (DateTime.Now > parsedDate)
+            {
+                return Json("Введите дату относящуюся к будущему",
+                    JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+                return Json(true, JsonRequestBehavior.AllowGet);
+            }
         }
     }
 }
