0ac0aa4d224bc452815a21c96d476239a1058176
Sergey Petrochenko <proffsneip@gmail.com>
2018-11-22 00:37:54 +0300

    BT-6: added data context.

---
+++ BestTours/BT.DataAccess/Context/ApplicationContext.cs
@@ -0,0 +1,26 @@
+﻿using System.Data.Entity;
+using BT.Dom.Entities;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace BT.DataAccess.Context
+{
+    public class ApplicationContext : IdentityDbContext<User>
+    {
+        public ApplicationContext(string connectionString) : base(connectionString)
+        { }
+
+        public DbSet<Tour> Tours { get; set; }
+        public DbSet<ClientProfile> ClientProfiles { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>().ToTable("Users");
+            modelBuilder.Entity<IdentityRole>().ToTable("Roles");
+            modelBuilder.Entity<IdentityUserRole>().ToTable("UserRoles");
+            modelBuilder.Entity<IdentityUserClaim>().ToTable("UserClaims");
+            modelBuilder.Entity<IdentityUserLogin>().ToTable("UserLogins");
+        }
+    }
+}
