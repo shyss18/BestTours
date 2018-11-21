9ce1abb5c5c0eb297b3a6cc2ed4f3906f7c451a0
Sergey Petrochenko <proffsneip@gmail.com>
2018-11-21 18:44:28 +0300

    BT-6: added user validation.

--- BestTours/BT.Web/Models/AccountModel.cs
+++ BestTours/BT.Web/Models/AccountModel.cs
@@ -1,15 +1,25 @@
-﻿namespace BT.Web.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BT.Web.Models
 {
     public class AccountModel
     {
+        [Display(Name = "Имя")]
+        [Required(ErrorMessage = "Данное поле не заполнено!")]
         public string FirstName { get; set; }
 
+        [Display(Name = "Фамилия")]
+        [Required(ErrorMessage = "Данное поле не заполнено!")]
         public string LastName { get; set; }
 
         public string NickName { get; set; }
 
+        [Display(Name = "Количество валюты")]
+        [Required(ErrorMessage = "Данное поле не заполнено!")]
+        [Range(0, 5000, ErrorMessage = "Слишком большое значение или отрицательное значение")]
         public decimal Amount { get; set; }
 
+        [Display(Name = "Email")]
         public string Email { get; set; }
     }
 }
